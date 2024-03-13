345a144c931dd60d015e1c91d39dc860c582ff8a
Anudeep2311 <anudeeplohghavkar2311@gmail.com>
2024-03-13 20:03:49 +0530

    Final Changes Completed

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -7,18 +7,6 @@ String apiKey = dotenv.env['API_KEY']!;
 class ApiService {
   final Dio _dio = Dio();
 
-  // Future<void> getUserData() async {
-  //   try {
-  //     Response response = await _dio.get('https://reqres.in/api/users');
-  //     return response;
-  //   } catch (error) {
-  //     print('Error fetching data: $error');
-  //     return Response(
-  //         requestOptions: RequestOptions(
-  //             path: ''));
-  //   }
-  // }
-
   Future<UserModel> fetchDataForAddress(String address) async {
     try {
       Response response = await _dio.get('https://reqres.in/api/users');
